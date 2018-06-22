410a2d9ce791911c8a306526530ade6ea8105ae4
Mary Warrick <mewarrick@gmail.com>
2018-06-22 17:11:41 -0500

    for merge

---
+++ resources.js
@@ -0,0 +1,242 @@
+
+//category: CSS/Styled components
+
+const resourcesPath1 = [
+    {
+    url : 'https://www.styled-components.com/docs/basics' ,
+    name : 'styled components docs' ,
+    type : 'website'
+    },
+    {
+      url : 'https://medium.com/styled-components/styled-components-getting-started-c9818acbcbbd',
+      name :  'styled components getting started',
+      type : 'article'
+
+    },
+    {
+      url: 'https://medium.freecodecamp.org/a-5-minute-intro-to-styled-components-41f40eb7cd55',
+      name: '5-minute to Styled Components',
+      type: 'article'
+    },
+    {
+      url: 'https://youtu.be/bIK2NwoK9xk',
+      name: 'styling react components',
+      type: 'video'
+    }
+
+],
+
+
+//category: javascript
+resourcesPath2 = [
+  {
+    url: 'http://crockford.com/javascript/',
+    name: 'Javascript Encyclopedia',
+    type: 'website'
+  },
+  {
+    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide',
+    name: 'MDN Javascript Guide',
+    type: 'documentation'
+  },
+  {
+    url: 'https://eloquentjavascript.net/Eloquent_JavaScript.pdf',
+    name: 'Eloquent Javascript',
+    type: 'pdf'
+  },
+  {
+    url: 'https://www.fullstackacademy.com/jumpstart-online',
+    name: 'Javascript Jumpstart',
+    type: 'web-course'
+  }
+]
+
+//es6, javascript
+
+resourcesPath3 = [
+  {
+    url: 'http://es6-features.org/#Constants',
+    name: 'ES6.org',
+    type: 'documentation'
+  },
+  {
+    url: 'https://es6.io/',
+    name: 'Wes Bos ES6 for everyone',
+    type: 'web-course'
+  },
+  {
+    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions',
+    name: 'Arrow Functions MDN',
+    type: 'website'
+  },
+  {
+    url: 'https://davidwalsh.name/spread-operator',
+    name: '6 great uses of the spread operator',
+    type: 'article'
+  }
+
+
+]
+
+//neo4j
+const resourcePath4 = [
+  {
+    url: 'https://neo4j.com/docs/cypher-refcard/current/?ref=browser-guide',
+    name: 'Neo4j Cypher Refcard',
+    type: 'documentation'
+  },
+  {
+    url: 'https://neo4j.com/neoassets/graphbooks/Graph_Databases_2e_Neo4j.pdf',
+    name: "O'Reilly Graph Databases",
+    type: 'pdf'
+  },
+  {
+    url: 'https://neo4j.com/developer/',
+    name: 'Neo4j developer site',
+    type: 'website'
+  },
+  {
+    url: 'https://neo4j.com/sandbox-v2/',
+    name: 'Neo4J sandbox',
+    type: 'tutorial'
+  }
+]
+
+//sequelize
+const resourcesPath5 = [
+  {
+    url: 'http://sequelize.readthedocs.io/en/v3/',
+    name: 'Sequelize Documentation',
+    type: 'documentation'
+  },
+  {
+    url: 'https://medium.com/@jasmine.esplago.munoz/feeling-the-magic-with-sequelize-magic-methods-e9cc89ecdcc5',
+    name: 'Feeling the Magic with Sequelize Magic Methods',
+    type: 'article'
+  },
+  {
+    url: 'https://gist.github.com/elliette/20ddc4e827efd9d62bc98752e7a62610',
+    name: 'Defining many-to-many Association Sequelize',
+    type: 'gist'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=Bv07mHw32TQ',
+    name: 'Creating Models with Sequelize',
+    type: 'video'
+  }
+]
+
+
+//Express
+const resourcesPath6 = [
+  {
+    url: 'https://expressjs.com/',
+    name: 'Express Documentation',
+    type: 'documentation'
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=gnsO8-xJ8rs',
+    name: 'Express js Crash Course',
+    type: 'video'
+  },
+  {
+   url: 'https://medium.freecodecamp.org/getting-off-the-ground-with-expressjs-89ada7ef4e59',
+   name: 'Getting off the Ground with Express.js',
+   type: 'article'
+  },
+  {
+   url: 'https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b',
+   name: 'Build a Weather Website in 30 Minutes with Node.js and Express',
+   type: 'article'
+  }
+]
+
+//html
+
+
+
+
+
+
+
+// (Python101:Path {name:'Beginner Python',description:'hello world',category:'python',level:'beginner',status:'public'}),
+
+// (DataScience:Path {name:'Data Science',description:'doing stuff with data',level:'beginner',status:'public'}),
+
+// (rpath:Path {name:'All About React', level:'intermediate',status:'public', createdDate:timestamp()}),
+
+// (styled:Path {name: 'All about styled components', description: 'learn about how to style CSS', level:'Beginner', status: 'public', createdDate:timestamp()}),
+
+// (jsbasic:Path {name: 'Basic JavaScript', description: 'intro course', level: 'Beginner', status: 'public', createdDate:timestamp()}),
+
+// (espath:Path {name: 'ES6 Highlights', description: 'useful tips for es6', level: 'Intermediate', status: 'public', createdDate:timestamp()}),
+
+// (neopath:Path {name: 'getting started with neo4j', description: 'a great graph database', level: 'Beginner', status: 'public', createdDate:timestamp()}),
+
+// (seqpath:Path {name: 'Sequelize Basics', description: 'learn about this relational database', level: 'Beginner', status: 'public', createdDate:timestamp()}),
+
+// (exprspath:Path {name: 'Make your own server', description: 'learn how to set up a server useing Express.js', level: 'Beginner', status: 'public', createdDate:timestamp()})
+
+// (htmlpath:Path {name:'HTML Starter Kit', description: 'intro to html', level:'Beginner', status: 'public', createdDate:timestamp()})
+
+
+
+
+
+
+
+
+
+
+const resourcesPath7 = [
+  {
+    url: 'https://www.w3schools.com/Html/',
+    name: 'HTML5 Tutorial',
+    type: 'tutorial'
+  },
+  {
+    url: 'https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Getting_started',
+    name: 'Getting started with HTML',
+    type: 'article'
+  },
+  {
+    url: 'https://www.codecademy.com/learn/learn-html',
+    name: 'Learn HTML',
+    type: 'web-course'
+  },
+  {
+    url: 'https://html.com/html5/',
+    name: "HTML5: What's New in the Latest Version of HTML",
+    type: 'article'
+  },
+  {
+
+  }
+
+]
+
+
+// Run these in next query!!
+
+// add review to dstk by chan
+// MATCH (u:User {name:'Chan'}), (r:Resource {name:'Data Science Toolkit'})
+// CREATE (a:Review {score:2,comments:'Site is frequently offline'}),
+// (u)-[:REVIEWS]->(a)-[:REVIEWS]->(r)
+
+// // add review to dstk by Mary
+// MATCH (u:User {name:'Mary'}), (r:Resource {name:'Data Science Toolkit'})
+// CREATE (a:Review {score:4,comments:'Very useful!'}),
+// (u)-[:REVIEWS]->(a)-[:REVIEWS]->(r)
+
+// // add categories
+// MATCH (p:Path {name:'Data Science'}), (r:Resource {name:'Data Science Toolkit'})
+// CREATE
+
+
+
+
+
+
+
+
+// creating more stuff
