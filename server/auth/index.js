f19b0606d270b8614eb89022b8dcd3af96a94eeb
J-Hust <33457091+J-Hust@users.noreply.github.com>
2018-06-26 17:51:06 -0500

    passwords are now salted

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,26 +1,34 @@
 const router = require('express').Router()
 const User = require('../db/models/user')
 module.exports = router
+const crypto = require('crypto')
 
-let neo4j = require('neo4j-driver').v1;
-let driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "1234"))
-let session = driver.session();
+let neo4j = require('neo4j-driver').v1
+let driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '1234'))
+let session = driver.session()
 
 router.post('/signup', async (req, res, next) => {
   try {
-
     const name = req.body.name
     const email = req.body.email
-    const pass = req.body.password
+    const salt = crypto.randomBytes(16).toString('base64')
+    const pass = crypto
+      .createHash('RSA-SHA256')
+      .update(req.body.password)
+      .update(salt)
+      .digest('hex')
 
-    console.log('in route: name', name, 'email', email, 'pass', pass)
     const query = `
-    CREATE (newuser:User {name: {name}, email: {email}, password: {password}, googleId: '', createdDate: timestamp(), isAdmin: false, id: 2})
+    CREATE (newuser:User {name: {name}, email: {email}, password: {password}, googleId: '', createdDate: timestamp(), isAdmin: false, salt: {salt}})
     RETURN newuser
   `
 
-    const response = await session.run(query, {name: name, email: email, password: pass})
-    console.log('user coming back from db', response.records[0]._fields[0].properties)
+    const response = await session.run(query, {
+      name: name,
+      email: email,
+      password: pass,
+      salt: salt
+    })
 
     const user = response.records[0]._fields[0].properties
     req.login(user, err => (err ? next(err) : res.json(user)))
@@ -34,51 +42,48 @@ router.post('/signup', async (req, res, next) => {
 })
 
 router.post('/login', async (req, res, next) => {
-  try{
+  try {
+    const name = req.body.name
+    const pass = req.body.password
 
-  const name = req.body.name
-  const pass = req.body.password
-
-  const query = `
+    //check if user exists
+    let query = `
     MATCH (u:User)
-    WHERE u.name = {name} and u.password = {password}
+    WHERE u.name = {name}
     RETURN properties(u)
   `
 
-  const response = await session.run(query, {name: name, password: pass})
-  console.log('response in login route', response.records[0]._fields[0])
-  const user = response.records[0]._fields[0]
-  console.log('user in login route', user)
-  req.login(user, err => (err ? next(err) : res.json(user)))
+    let response = await session.run(query, {name: name})
+    let user = response.records[0]._fields[0]
 
+    //if the pw is salted in the database
+    if (user.salt) {
+      const saltedPW = crypto
+        .createHash('RSA-SHA256')
+        .update(pass)
+        .update(user.salt)
+        .digest('hex')
 
-} catch (err) {
-  res.status(401).send('Wrong username or password')
-}
-  // const user = await User.findOne({where: {email: req.body.email}})
-  // if (!user) {
-  //   console.log('No such user found:', req.body.email)
-  //   res.status(401).send('Wrong username and/or password')
-  // } else if (!user.correctPassword(req.body.password)) {
-  //   console.log('Incorrect password for user:', req.body.email)
-  //   res.status(401).send('Wrong username and/or password')
-  // } else {
-  //   req.login(user, err => (err ? next(err) : res.json(user)))
-  // }
+      query = `MATCH (u:User)
+        WHERE u.name = {name} and u.password = {pw}
+        RETURN properties(u)
+      `
+      response = await session.run(query, {name: name, pw: saltedPW})
+    } else {
+      // seed file user without salted pw
+      query = `MATCH (u:User)
+        WHERE u.name = {name} and u.password = {pw}
+        RETURN properties(u)
+      `
+      response = await session.run(query, {name: name, pw: pass})
+    }
+
+    user = response.records[0]._fields[0]
+    req.login(user, err => (err ? next(err) : res.json(user)))
+  } catch (err) {
+    res.status(401).send('Wrong username or password')
+  }
 })
-
-// router.post('/signup', async (req, res, next) => {
-//   try {
-//     const user = await User.create(req.body)
-//     req.login(user, err => (err ? next(err) : res.json(user)))
-//   } catch (err) {
-//     if (err.name === 'SequelizeUniqueConstraintError') {
-//       res.status(401).send('User already exists')
-//     } else {
-//       next(err)
-//     }
-//   }
-// })
 
 router.post('/logout', (req, res) => {
   req.logout()
