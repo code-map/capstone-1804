12a04ba8ec72249121cc4207483fb9c122d4e349
Chan Youn <chansiky@gmail.com>
2018-06-26 18:03:13 -0500

    Update records-reducer.js

--- server/api/records-reducer.js
+++ server/api/records-reducer.js
@@ -15,10 +15,62 @@ const recordsReducer = (records) => {
 }
 
 module.exports = recordsReducer
-
 /*
-EXAMPLE INPUT:
+EXAMPLE INPUT: queryResult.records =
+[
+  {
+    keys: [
+      "name",
+      "owner",
+      "reviewCount",
+      "userCount",
+      "rating"
+    ],
+    length: 5,
+    _fields: [
+      "ES6 Highlights",
+      "Chan",
+      {
+        low: 4,
+        high: 0
+      },
+      {
+        low: 2,
+        high: 0
+      },
+      4.5
+    ],
+    _fieldLookup: {
+      name: 0,
+      owner: 1,
+      reviewCount: 2,
+      userCount: 3,
+      rating: 4
+    }
+  },
+  .
+  .
+  .
+]
 
-EXAMPLE OUTPUT:
 
+EXAMPLE OUTPUT: returnedArray = 
+[
+  {
+    name: "All About React",
+    owner: "dragon-slayer",
+    reviewCount: {
+      low: 9,
+      high: 0
+    },
+    userCount: {
+      low: 3,
+      high: 0
+    },
+    rating: 4
+    },
+  .
+  .
+  .
+]
 */
