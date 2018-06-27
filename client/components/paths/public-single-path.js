f06339a0a26f7a654614ba48108e538bbfb14679
Mary Warrick <mewarrick@gmail.com>
2018-06-27 16:10:56 -0500

    cleaned up the search bar - there is still an interaction bug with the stars component, might need to find a new method for stars.  Also, hyperlink will be added when the single path components are up and running

---
+++ client/components/paths/public-single-path.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PathProgress from './path-progress'
+
+import { getSinglePathThunk } from '../../store'
+
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Collapse from '@material-ui/core/Collapse'
+import ExpandLess from '@material-ui/icons/ExpandLess'
+import ExpandMore from '@material-ui/icons/ExpandMore'
+import Button from '@material-ui/core/Button'
+
+const styles = {
+  container: {
+    backgroundColor: 'white',
+    borderWidth: '1px',
+    borderColor: '#efefef',
+    borderStyle: 'solid'
+  }
+}
+
+class PublicSinglePath extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      selectedItems: []
+    }
+  }
+
+  componentDidMount = () => {
+    const {pathName} = this.props.match.params
+    getPath('test')
+  }
+
+  handleDropdownClick = (step) => {
+    this.setState((prevState) => {
+      return {
+        selectedItems: prevState.selectedItems.filter((el) => el !== step)
+      }
+    })
+  }
+
+  handleCollapseClick = (step) => {
+    this.setState((prevState) => {
+      return {
+        selectedItems: prevState.selectedItems.concat([step])
+      }
+    })
+  }
+
+
+
+
+  render(){
+    const path = this.props.path
+
+    return (
+      <div>
+        <h3>{path.details.properties.name}</h3>
+        <div style={styles.container}>
+          <List>
+            { path.steps.length > 1 &&
+              path.steps.map(step => {
+                const stepUrl = step.resource.properties.url
+                return (
+                <div key={stepUrl}>
+                  <ListItem
+                    key={stepUrl}
+                    role={undefined}
+                    dense
+                    button
+                    disableRipple
+                  >
+                    {
+                      step.resource.properties.imageUrl ? (
+                        <img src={step.resource.properties.imageUrl} width={75} />
+                      ) : (
+                        <img src="../../default.png" width={75} />
+                      )
+                    }
+
+                    <ListItemText primary={step.resource.properties.name} />
+
+                    {this.state.selectedItems.indexOf(stepUrl) !== -1 ?
+                      <ExpandLess
+                        onClick={() => this.handleDropdownClick(stepUrl)}
+                      /> :
+                      <ExpandMore
+                        onClick={() => this.handleCollapseClick(stepUrl)}
+                      />
+                    }
+
+                  </ListItem>
+
+                  <Collapse
+                    in={this.state.selectedItems.indexOf(stepUrl) !== -1}
+                    timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      <ListItem button>
+                        <p>In the dropdown for "{step.resource.properties.name}"</p>
+                      </ListItem>
+                    </List>
+                  </Collapse>
+
+                  </div>
+                )
+            } ) }
+          </List>
+
+        </div>
+
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log('path state', state)
+  return {
+    completedSteps: state.pathReducer.singlePath
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getPath: (name) => {
+      dispatch(getSinglePathThunk(name))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PublicSinglePath)
--- client/components/paths/stars.js
+++ client/components/paths/stars.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import StarRatingComponent from 'react-star-rating-component';
+import styled from "styled-components";
+
+
+const StarBox = styled.div`
+  z-Index: 2;
+`
 
 const Stars = (props) => {
   const {value} = props
   return(
+    <StarBox>
     <StarRatingComponent
           name="stars"
           editing={false}
           starCount={5}
           value={value}
         />
+    </StarBox>
   )
 }
 
