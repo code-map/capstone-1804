6ac1048305c949acc8633e2616290313267fadf8
J-Hust <33457091+J-Hust@users.noreply.github.com>
2018-06-27 17:36:32 -0500

    category-single updates when new category is clicked in category dropdown

--- client/components/category/category-single.js
+++ client/components/category/category-single.js
@@ -1,101 +1,112 @@
-import React, { Component } from 'react'
-import { CategoryAllPaths, CategorySearch, CategoryPopularPaths } from './'
-import { connect } from 'react-redux'
-import { createGetSingleCategoryThunk } from '../../store'
-import styled from "styled-components";
+import React, {Component} from 'react'
+import {CategoryAllPaths, CategorySearch, CategoryPopularPaths} from './'
+import {connect} from 'react-redux'
+import {createGetSingleCategoryThunk} from '../../store'
+import styled from 'styled-components'
 import {Link} from 'react-router-dom'
-import Grid from '@material-ui/core/Grid';
-import { SearchAny } from '../'
-
+import Grid from '@material-ui/core/Grid'
+import {SearchAny} from '../'
 
 class CategorySinglePage extends Component {
-  constructor(){
+  constructor() {
     super()
   }
 
-  async componentDidMount(){
-    const { categoryName } = this.props.match.params
+  async componentDidMount() {
+    const {categoryName} = this.props.match.params
     await this.props.getAllItemsInCategory(categoryName)
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.match.params !== this.props.match.params) {
+      const {categoryName} = nextProps.match.params
+      this.props.getAllItemsInCategory(categoryName)
+    }
+  }
 
   render() {
-    const { paths, resources, name, url } = this.props.categoryItems
-    if(paths.length) {
-      return(
+    const {paths, resources, name, url} = this.props.categoryItems
+    if (paths.length) {
+      return (
         <Container>
           <HeaderSearchContainer>
-          <Header>{name.trim()}</Header>
-          <SearchBox>
-            <SearchAny category={name}/>
-        </SearchBox>
-        </HeaderSearchContainer>
-        <HeadlineCol>
-          <SubHeader>{`Popular paths in ${name}`}</SubHeader>
-        <ScrollBox>
-          <CategoryAllPaths paths={paths}/>
-        </ScrollBox>
-        </HeadlineCol>
-        <Headline>
-          <Grid container spacing={24}>
+            <Header>{name.trim()}</Header>
+            <SearchBox>
+              <SearchAny category={name} />
+            </SearchBox>
+          </HeaderSearchContainer>
+          <HeadlineCol>
+            <SubHeader>{`Popular paths in ${name}`}</SubHeader>
+            <ScrollBox>
+              <CategoryAllPaths paths={paths} />
+            </ScrollBox>
+          </HeadlineCol>
+          <Headline>
+            <Grid container spacing={24}>
               <Grid item xs={12} sm={6}>
-            <ListContainer>
-            <SubHeader style={{marginTop:'15px'}}>{`Popular resources in ${name}`}</SubHeader>
-              <div>
-              {
-                resources.map((resource) => {
-                  return <p key={resource.name}><a  href={resource.url}>{resource.name}</a></p>
-                })
-              }
-              </div>
-            </ListContainer>
+                <ListContainer>
+                  <SubHeader
+                    style={{marginTop: '15px'}}
+                  >{`Popular resources in ${name}`}</SubHeader>
+                  <div>
+                    {resources.map(resource => {
+                      return (
+                        <p key={resource.name}>
+                          <a href={resource.url}>{resource.name}</a>
+                        </p>
+                      )
+                    })}
+                  </div>
+                </ListContainer>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <ListContainer>
+                  <SubHeader
+                    style={{marginTop: '15px'}}
+                  >{`All paths in ${name}`}</SubHeader>
+                  <div>
+                    {paths.map(path => {
+                      return (
+                        <p key={path.name}>
+                          <Link to={`/${path.name}`}>{path.name}</Link>
+                        </p>
+                      )
+                    })}
+                  </div>
+                </ListContainer>
+              </Grid>
             </Grid>
-          <Grid item xs={12} sm={6}>
-          <ListContainer>
-          <SubHeader style={{marginTop:'15px'}}>{`All paths in ${name}`}</SubHeader>
-              <div>
-              {
-                paths.map((path) => {
-                  return(<p key={path.name}><Link to={`/${path.name}`} >{path.name}</Link></p>)
-                })
-              }
-              </div>
-            </ListContainer>
-            </Grid>
-            </Grid>
-            </Headline>
+          </Headline>
         </Container>
       )
-    }else{
+    } else {
       return <p>loading</p>
     }
-
   }
 }
 
-
-const mapState = (state) => {
-  return({
+const mapState = state => {
+  return {
     categoryItems: state.singleCategory
-  })
+  }
 }
 
-const mapDispatch = (dispatch) => {
-  return({
-    getAllItemsInCategory : (categoryName) => {
+const mapDispatch = dispatch => {
+  return {
+    getAllItemsInCategory: categoryName => {
       return dispatch(createGetSingleCategoryThunk(categoryName))
     }
-  })
+  }
 }
 
 const Header = styled.h1`
-  color: #55288B;
+  color: #55288b;
   font-family: Helvetica;
   font-weight: 400;
   font-size: 4em;
-  line-height: .01;
+  line-height: 0.01;
   display: inline-block;
-  margin-right: 20px
+  margin-right: 20px;
 `
 
 const HeaderSearchContainer = styled.div`
@@ -103,7 +114,7 @@ const HeaderSearchContainer = styled.div`
   width: 60vw;
   justify-content: space-between;
   align-self: center;
-  margin-top: -60px
+  margin-top: -60px;
 `
 
 const SubHeader = styled.p`
@@ -111,7 +122,7 @@ const SubHeader = styled.p`
   font-family: Helvetica;
   font-size: 1.2em;
   font-weight: 200;
-  margin: 0
+  margin: 0;
 `
 
 const Container = styled.div`
@@ -125,50 +136,48 @@ const Container = styled.div`
 `
 
 const Headline = styled.div`
-margin: 10px;
-width: auto;
-height: auto;
-box-shadow: 0 1px 1px rgba(0,0,0,0.2), 0 1px 2px rgba(0,0,0,0.13);
-padding-left: 20px;
-overflow:auto;
-background-color: rgb(232, 194, 239, .2)
+  margin: 10px;
+  width: auto;
+  height: auto;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 1px 2px rgba(0, 0, 0, 0.13);
+  padding-left: 20px;
+  overflow: auto;
+  background-color: rgb(232, 194, 239, 0.2);
 `
 
 const HeadlineCol = styled.div`
-margin: 20px;
-width: 90vw;
-height: 250px;
-box-shadow: 0 1px 1px rgba(0,0,0,0.2), 0 1px 2px rgba(0,0,0,0.13);
-background-color: rgb(232, 194, 239, .2);
-align-self: center;
-display: flex;
-flex-direction: column;
-padding: 15px;
-overflow: hidden
-
+  margin: 20px;
+  width: 90vw;
+  height: 250px;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 1px 2px rgba(0, 0, 0, 0.13);
+  background-color: rgb(232, 194, 239, 0.2);
+  align-self: center;
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  overflow: hidden;
 `
 
 const ScrollBox = styled.div`
-width: 90vw;
-height: 200px;
-overflow: scroll;
-display: flex;
-flex-wrap: nowrap;
-justify-content: center;
-align-self: center
-
+  width: 90vw;
+  height: 200px;
+  overflow: scroll;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-self: center;
 `
 
 const SearchBox = styled.div`
-margin: 10px;
-width: 35vw;
-align-self: center;
-height: auto;
-box-shadow: 0 1px 1px rgba(0,0,0,0.2), 0 1px 2px rgba(0,0,0,0.13);
-padding-left: 20px;
-padding-top: 0;
-background-color: white;
-display: inline-block
+  margin: 10px;
+  width: 35vw;
+  align-self: center;
+  height: auto;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 1px 2px rgba(0, 0, 0, 0.13);
+  padding-left: 20px;
+  padding-top: 0;
+  background-color: white;
+  display: inline-block;
 `
 
 const ListContainer = styled.div`
@@ -177,18 +186,6 @@ const ListContainer = styled.div`
 `
 
 export default connect(mapState, mapDispatch)(CategorySinglePage)
-
-
-
-
-
-
-
-
-
-
-
-
 
 //ORIGINAL SCROLL BOX, DO NOT DELETE!!!!!
 // const ScrollBox = styled.div`
