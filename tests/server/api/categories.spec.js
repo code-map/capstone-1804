ea83d072799f651f3a66a1dae51456f12b49e01b
J-Hust <33457091+J-Hust@users.noreply.github.com>
2018-06-28 16:54:37 -0500

    update tests

---
+++ tests/server/api/categories.spec.js
@@ -0,0 +1,38 @@
+// NOTE: THESE TESTS ASSUME DB IS ALREADY SEEDED!
+
+const chai = require('chai');
+const expect = chai.expect;
+// const chaiThings = require('chai-things');
+// chai.use(chaiThings);
+let neo4j = require('neo4j-driver').v1;
+let driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "1234"))
+let session = driver.session();
+const app = require('../../../server/index.js');
+const agent = require('supertest')(app);
+
+describe('Categories API Routes', () => {
+  before(async () => {
+    //re-seed db?
+  })
+
+  describe('GET api/categories/all/parent', () => {
+    it('returns all language categories', async () => {
+      const response = await agent.get('/categories/all/parent')
+      .expect(200)
+    })
+  })
+
+  describe('/:categoryName/popular-paths', () => {
+    it('returns the correct paths and other info', async () => {
+      const response = await agent.get('/Javascript/popular-paths')
+      .expect(200)
+    })
+  })
+
+  describe('/:categoryName/search', () => {
+    it('returns all resources and paths within queried category', async () => {
+      const response = await agent.get('/Javascript/search')
+      .expect(200)
+    })
+  })
+})
