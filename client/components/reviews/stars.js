02e76b507b8f1dd24e032d7952a441b82167ffb6
chansiky <chansiky@gmail.com>
2018-06-29 11:23:41 -0500

    unrated stars

--- client/components/reviews/stars.js
+++ client/components/reviews/stars.js
@@ -6,10 +6,13 @@ import styled from "styled-components";
 const StarBox = styled.div`
   z-Index: 2;
 `
-
+/*
+ * the 'unrated' prop can be passed down as true, or a falsey 'value' will 
+ * make the stars display with a disabled color.
+ */
 const Stars = (props) => {
-  const {value} = props
-  const emptyColor = value ? "#a5a5a5" : "#c8c8c8"
+  const {value, unrated} = props
+  const emptyColor = (!value || unrated) ?  "#c8c8c8" : "#a5a5a5"
   const filledColor = "#e5aa3d"
   return(
     <StarBox>
