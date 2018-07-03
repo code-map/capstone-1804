f2ede6ee6b8b54206b0dc2ec35a4d61aaebb9822
Jami Gibbs <jami0821@gmail.com>
2018-07-03 11:12:56 -0500

    Removing testing route

--- client/routes.js
+++ client/routes.js
@@ -19,7 +19,6 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route exact path="/dragTest" component={DraggableListContainer} />
         <Route exact path="/" component={HomePage} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
