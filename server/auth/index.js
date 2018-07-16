e225f809ebaaf26a23340adedf910fbb954cd858
Jami Gibbs <jami0821@gmail.com>
2018-07-15 20:11:45 -0500

    Laying down groundwork for testing user authed routes

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const User = require('../db/models/user')
-module.exports = router
 const crypto = require('crypto')
 
 // let neo4j = require('neo4j-driver').v1
@@ -52,12 +51,10 @@ router.post('/login', async (req, res, next) => {
     let query = `
     MATCH (u:User)
     WHERE u.name = {name}
-    RETURN u
-  `
+    RETURN u`
 
     let response = await session.run(query, {name: name})
     let user = response.records[0]._fields[0].properties
-
 
     //if the pw is salted in the database
     if (user.salt) {
@@ -69,19 +66,20 @@ router.post('/login', async (req, res, next) => {
 
       query = `MATCH (u:User)
         WHERE u.name = {name} and u.password = {pw}
-        RETURN properties(u)
-      `
+        RETURN u`
+
       response = await session.run(query, {name: name, pw: saltedPW})
-    } else {
-      // seed file user without salted pw
+    } else if (!user.salt){
+      //seed file user without salted pw
       query = `MATCH (u:User)
-        WHERE u.name = {name} and u.password = {pw}
-        RETURN properties(u)
-      `
-      response = await session.run(query, {name: name, pw: pass})
+        WHERE u.name = {name} and u.password = {pass}
+        RETURN u`
+
+      response = await session.run(query, {name, pass})
     }
 
-    user = response.records[0]._fields[0]
+    user = response.records[0]._fields[0].properties
+
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     res.status(401).send('Wrong username or password')
@@ -99,3 +97,5 @@ router.get('/me', (req, res) => {
 })
 
 router.use('/google', require('./google'))
+
+module.exports = router
