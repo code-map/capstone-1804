cc280ca3013fb73049324fd1ec47b55251568821
Mary Warrick <mewarrick@gmail.com>
2018-11-19 11:06:43 -0600

    clean up
    removed commented code and console logs

--- server/api/search.js
+++ server/api/search.js
@@ -15,25 +15,13 @@ router.post('/', async (req, res, next) => {
     RETURN c AS matches`
     const response = await session.run(query, {searchString})
     const fuzzyMatch = response.records
-    //console.log('FUZZY', fuzzyMatch)
     res.json(fuzzyMatch)
   }catch(err){
     next(err)
   }
 })
 
-// router.put('/', async (req, res, next) => {
-//     var randomId = shortid.generate()
-//     const query = `MATCH p = (n)-[*]->(END)
-//     FOREACH (n IN nodes(p) | SET n.test={randomId})`
-//     const response = await session.run(query, {randomId()})
-//     res.json(response.records)
-// })
-
 
 module.exports = router
 
 
-// `MATCH (n)
-//     WHERE toLower(n.slug) CONTAINS toLower({searchString})
-//     return n`
